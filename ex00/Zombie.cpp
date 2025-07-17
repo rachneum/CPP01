c35f36712636d7e284a02154eaa04c797b1240e6
Rachel Neumann <raneuman@mi-r2-p2.s19.be>
2025-07-17 20:20:53 +0200

    test

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,21 +6,24 @@
 /*   By: raneuman <raneuman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 18:25:25 by raneuman          #+#    #+#             */
-/*   Updated: 2025/07/17 20:03:39 by raneuman         ###   ########.fr       */
+/*   Updated: 2025/07/17 20:19:37 by raneuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string zombieName) : name(zombieName) {
+Zombie::Zombie(std::string zombieName) : name(zombieName)
+{
+    name = zombieName;
     std::cout << "Zombie " << name << " has been constructed." << std::endl;
-}
-
-Zombie::~Zombie() {
-    std::cout << "Zombie " << name << " has been destroyed." << std::endl;
 }
 
 void	Zombie::announce(void)
 {
 	std::cout << name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie " << name << " has been destroyed." << std::endl;
 }
